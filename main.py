bce5f80654a9d0e6d7eda16ef7508a4c483a1317
Algokodabra <algokodabra@gmail.com>
2022-12-14 11:47:02 +0400

    Refactoring.

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import datetime
-import threading
 import numpy
 import cv2
 import pybullet
 import pybullet_data
-from flask import Flask
-from flask.wrappers import Response
+import flask
+import waitress
 
 # Setup parameters.
-host = "192.168.0.102"  # IP-address of the streaming server.
-port = 8080  # Port number of the streaming server.
-frameSize = (256, 256)  # Resolution of the syntheric camera.
+frameSize = (640, 480)  # Resolution of the syntheric camera.
 useRealTimeSimulation = False  # Indicates to call `stepSimulation()` method automatically as fast as possible.
 
-app = Flask(__name__)
-app.secret_key = "131200e9e5bc8f3f2a5b88d844f5ea45820da7cb0e026b5b56bc2e61dc7cfb8b"
-
-# Initialize a lock used to ensure thread-safe exchanges of the frames. It's required to support multiple clients.
-lock = threading.Lock()
+renderingTimeSeconds = 0.0
+renderedFramesNumber = 0
+fps = 0.0
 
 # Initialize pybullet environment.
 pybullet.connect(pybullet.DIRECT)  # GUI/DIRECT
@@ -25,43 +20,79 @@ pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
 pybullet.setGravity(0.0, 0.0, -10.0)
 pybullet.setRealTimeSimulation(useRealTimeSimulation)
 pybullet.loadURDF("plane.urdf")
-pybullet.loadURDF("r2d2.urdf", (0.0, 0.0, 1.0), pybullet.getQuaternionFromEuler((0.0, 0.0, 0.0)))
+
+# TODO: Do I need this debug visualizer configuration? Seems it has no effect on performance.
+pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_GUI, 0)
+pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_SEGMENTATION_MARK_PREVIEW, 0)
+pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_DEPTH_BUFFER_PREVIEW, 0)
+pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_RGB_BUFFER_PREVIEW, 0)
+
+# Create robot and camera position matrix.
+robotPosition = (0.0, 0.0, 1.0)
+robotOrientation = pybullet.getQuaternionFromEuler((0.0, 0.0, 0.0))
+robot = pybullet.loadURDF("r2d2.urdf", robotPosition, robotOrientation)
 viewMatrix = pybullet.computeViewMatrix(
     cameraEyePosition=(2.0, 2.0, 2.0), cameraTargetPosition=(0.0, 0.0, 0.0), cameraUpVector=(0.0, 0.0, 1.0)
 )
 
+# Create Flask app.
+app = flask.Flask(__name__)
+# app.secret_key = "131200e9e5bc8f3f2a5b88d844f5ea45820da7cb0e026b5b56bc2e61dc7cfb8b"  # TODO: Do I need this?
+
 
 @app.route("/stream", methods=["GET"])
 def stream():
-    return Response(getFrame(), mimetype="multipart/x-mixed-replace; boundary=frame")
+    return flask.Response(streamingLoop(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
-def getFrame():
-    global lock
+@app.route("/reset", methods=["POST"])
+def reset():
+    pybullet.resetBasePositionAndOrientation(robot, robotPosition, robotOrientation)
+    return flask.Response()
 
+
+def captureFrame():
+    global renderingTimeSeconds, renderedFramesNumber, fps
+
+    startTime = datetime.datetime.now()
+
+    image = numpy.zeros((frameSize[0] * frameSize[1] * 4,))  # Empty debug image.
+    _, _, image, _, _ = pybullet.getCameraImage(
+        width=frameSize[0],
+        height=frameSize[1],
+        viewMatrix=viewMatrix,
+        shadow=False,
+        renderer=pybullet.ER_TINY_RENDERER,  # ER_TINY_RENDERER/ER_BULLET_HARDWARE_OPENGL
+    )
+    frame = numpy.reshape(image, (frameSize[1], frameSize[0], 4))[:, :, 2::-1]  # Invert channels order.
+    frame = cv2.normalize(frame, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+
+    information = f"FPS: {fps:.2f} / time: {datetime.datetime.now()}"
+    frame = cv2.putText(frame, information, (5, 15), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 255))
+
+    endTime = datetime.datetime.now()
+    renderingTimeSeconds += (endTime - startTime).total_seconds()
+    renderedFramesNumber += 1
+    fps = renderedFramesNumber / renderingTimeSeconds
+
+    return frame
+
+
+def streamingLoop():
     while True:
-        with lock:  # Wait until the lock is acquired.
-            if not useRealTimeSimulation:
-                pybullet.stepSimulation()
-            images = pybullet.getCameraImage(frameSize[0], frameSize[1], viewMatrix)
-            frame = numpy.reshape(images[2], (*frameSize, 4))[:, :, 2::-1]  # Invert channels order.
-            frame = cv2.normalize(frame, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+        if not useRealTimeSimulation:
+            pybullet.stepSimulation()
 
-            timeString = str(datetime.datetime.now())
-            frame = cv2.putText(frame, timeString, (5, 15), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 255))
+        frame = captureFrame()
 
-            isEncoded, encodedFrame = cv2.imencode(".jpg", frame)  # Encode the frame in JPEG format.
-            if not isEncoded:
-                continue
+        # Encode the frame in JPEG format.
+        isEncoded, encodedFrame = cv2.imencode(".jpg", frame)
+        if not isEncoded:
+            continue
 
         # Yield the output frame in the byte format.
-        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + bytearray(encodedFrame) + b"\r\n")
+        yield (b"--frame\r\n" b"Content-Type: image/jpg\r\n\r\n" + bytearray(encodedFrame) + b"\r\n")
 
 
-# waitress-serve --host='192.168.0.102' --call 'main:flaskApp'
-def flaskApp():
-    return app
-
-
-if __name__ == "__main__":
-    app.run(host, port)  # TODO: For debug only, do not use this method in production.
+# Start server listening at localhost:8080.
+waitress.serve(app)
