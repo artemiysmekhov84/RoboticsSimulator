ccd9ae42e1436061ceaf6dff9f5823260a5d84a6
Artemiy Smekhov <>
2023-01-06 18:50:31 +0400

    Refactoring.

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
+# Create Docker image based on Drake 1.11.0.
 FROM robotlocomotion/drake:1.11.0
 
 ADD main.py .
--- main.py
+++ main.py
@@ -1,28 +1,11 @@
-# TODO: Seems like all OS-level dependensies installed locally are not including into the final Docker image.
-# This is why deploy crashes on run with error "ImportError: libX11.so.6: cannot open shared object file".
-# Need to make Docker container including all required dependencies and push it to Git repository.
-# Required dependency list: libx11-6 libsm6 libglib2.0-0 [python-tk].
-# Google for "putting X11 into a docker container", "install python drake into docker".
-# apt-get update -y && apt-get upgrade -y && apt-get install -y --no-install-recommends xvfb xpra xserver-xephyr xinit xauth xclip x11-xserver-utils x11-utils libx11-6 libsm6 libglib2.0-0 python-tk
-
-# TODO: Add 'reset' endpoint.
-
-import subprocess
-
-print("========== CHECK IF libx11 IS INSTALLED ==========")
-subprocess.call("apt list --installed | grep libx11", shell=True)
+# TODO: Add "Reset" button.
 
 import numpy
-
-print("========== BEFORE IMPORT DRAKE ==========")
-
-from pydrake.geometry import Meshcat, MeshcatVisualizer, MeshcatParams
+from pydrake.geometry import Meshcat, MeshcatVisualizer
 from pydrake.multibody.parsing import Parser
 from pydrake.multibody.plant import AddMultibodyPlantSceneGraph
 from pydrake.systems.analysis import Simulator
 from pydrake.systems.framework import DiagramBuilder
-
-print("========== AFTER IMPORT DRAKE ==========")
 
 builder = DiagramBuilder()
 plant, sceneGraph = AddMultibodyPlantSceneGraph(builder, time_step=0.001)  # type: ignore
@@ -35,8 +18,7 @@ plant.GetJointByName("joint_0").set_default_angle(numpy.pi / 2.0)
 plant.GetJointByName("joint_1").set_default_angle(numpy.pi / 2.0)
 
 # Add visualizer to visualize the geometries.
-params = MeshcatParams(host="0.0.0.0", port=8000, web_url_pattern="https://{host}:{port}")
-meshcat = Meshcat(params)
+meshcat = Meshcat()
 MeshcatVisualizer.AddToBuilder(builder, sceneGraph, meshcat)  # type: ignore
 
 diagram = builder.Build()
